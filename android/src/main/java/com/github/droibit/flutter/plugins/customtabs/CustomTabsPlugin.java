9e47688971423ceb6748606dc18903182d40c207
kutear <git@kutear.com>
2021-10-13 02:16:06 +0800

    feat: update plugin to v2

--- android/src/main/java/com/github/droibit/flutter/plugins/customtabs/CustomTabsPlugin.java
+++ android/src/main/java/com/github/droibit/flutter/plugins/customtabs/CustomTabsPlugin.java
@@ -1,5 +1,6 @@
 package com.github.droibit.flutter.plugins.customtabs;
 
+import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
@@ -15,23 +16,33 @@ import java.util.HashMap;
 import java.util.Map;
 
 import io.flutter.Log;
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.embedding.engine.plugins.activity.ActivityAware;
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
+import io.flutter.plugin.common.BinaryMessenger;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.PluginRegistry;
 
 import static android.content.Intent.ACTION_VIEW;
 
-public class CustomTabsPlugin implements MethodChannel.MethodCallHandler {
-    private MethodChannel globalChannel;
+public class CustomTabsPlugin implements FlutterPlugin, ActivityAware, MethodChannel.MethodCallHandler {
+
+    private void register(BinaryMessenger messenger, Context context, Activity activity) {
+        final MethodChannel channel =
+                new MethodChannel(messenger, "com.github.droibit.flutter.plugins.custom_tabs");
+        channel.setMethodCallHandler(this);
+        this.context = context;
+        if (activity != null)
+            this.activity = activity;
+    }
 
     /**
      * Plugin registration.
      */
     public static void registerWith(PluginRegistry.Registrar registrar) {
-        final MethodChannel channel =
-                new MethodChannel(registrar.messenger(), "com.github.droibit.flutter.plugins.custom_tabs");
-        channel.setMethodCallHandler(new CustomTabsPlugin(channel, registrar));
-
+        final CustomTabsPlugin plugin = new CustomTabsPlugin();
+        plugin.register(registrar.messenger(), registrar.context(), registrar.activity());
     }
 
     private static final String KEY_OPTION = "option";
@@ -46,14 +57,16 @@ public class CustomTabsPlugin implements MethodChannel.MethodCallHandler {
 
     private static final String CODE_LAUNCH_ERROR = "LAUNCH_ERROR";
 
-    private final PluginRegistry.Registrar registrar;
+    private Launcher launcher;
+    private Activity activity;
+    private Context context;
 
-    private final Launcher launcher;
 
-    private CustomTabsPlugin(@NonNull MethodChannel channel, @NonNull PluginRegistry.Registrar registrar) {
-        this.registrar = registrar;
-        this.globalChannel = channel;
-        this.launcher = new Launcher(registrar.activeContext());
+    private Launcher getLauncher() {
+        if (launcher == null) {
+            launcher = new Launcher(activity);
+        }
+        return launcher;
     }
 
     @SuppressWarnings("unchecked")
@@ -66,7 +79,7 @@ public class CustomTabsPlugin implements MethodChannel.MethodCallHandler {
             case "launchNative":
                 launchNative(((Map<String, Object>) call.arguments), result);
                 break;
-            case "launchUrl" :
+            case "launchUrl":
                 launchUrl(((Map<String, Object>) call.arguments), result);
                 break;
             case "isSupportCustomTabs":
@@ -79,15 +92,15 @@ public class CustomTabsPlugin implements MethodChannel.MethodCallHandler {
     }
 
     private void isSupportCustomTabs(@NonNull MethodChannel.Result result) {
-        result.success(!launcher.getSupportCustomTabsPackages(registrar.context()).isEmpty());
+        result.success(!getLauncher().getSupportCustomTabsPackages(context).isEmpty());
     }
 
     private void launchUrl(@NonNull final Map<String, Object> args, @NonNull MethodChannel.Result result) {
         final Context context;
-        if (registrar.activity() != null) {
-            context = registrar.activity();
+        if (activity != null) {
+            context = activity;
         } else {
-            context = registrar.context();
+            context = this.context;
         }
         final Uri uri = Uri.parse(args.get(KEY_URL).toString());
         Intent intent = new Intent(ACTION_VIEW, uri);
@@ -101,15 +114,15 @@ public class CustomTabsPlugin implements MethodChannel.MethodCallHandler {
 
     private void launchNative(@NonNull final Map<String, Object> args, @NonNull MethodChannel.Result result) {
         final Context context;
-        if (registrar.activity() != null) {
-            context = registrar.activity();
+        if (activity != null) {
+            context = activity;
         } else {
-            context = registrar.context();
+            context = this.context;
         }
         final Uri uri = Uri.parse(args.get(KEY_URL).toString());
         final String title = String.valueOf(args.get(KEY_TITLE));
         final Map<String, Object> options = (Map<String, Object>) args.get(KEY_OPTION);
-        final CustomTabsIntent customTabsIntent = launcher.buildIntent(options);
+        final CustomTabsIntent customTabsIntent = getLauncher().buildIntent(options);
 
         Intent intent = new Intent(context, WebViewActivity.class);
         intent.putExtra(WebViewActivity.EXTRA_URL, uri.toString());
@@ -123,20 +136,50 @@ public class CustomTabsPlugin implements MethodChannel.MethodCallHandler {
     private void launch(@NonNull final Map<String, Object> args, @NonNull MethodChannel.Result result) {
         final Uri uri = Uri.parse(args.get(KEY_URL).toString());
         final Map<String, Object> options = (Map<String, Object>) args.get(KEY_OPTION);
-        final CustomTabsIntent customTabsIntent = launcher.buildIntent(options);
+        final CustomTabsIntent customTabsIntent = getLauncher().buildIntent(options);
 
         final Context context;
-        if (registrar.activity() != null) {
-            context = registrar.activity();
+        if (activity != null) {
+            context = activity;
         } else {
-            context = registrar.context();
+            context = this.context;
             customTabsIntent.intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         }
         try {
-            boolean launchResult = launcher.launch(context, uri, customTabsIntent);
+            boolean launchResult = getLauncher().launch(context, uri, customTabsIntent);
             result.success(launchResult);
         } catch (ActivityNotFoundException e) {
             result.error(CODE_LAUNCH_ERROR, e.getMessage(), null);
         }
+    }
+
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
+        register(binding.getBinaryMessenger(), binding.getApplicationContext(), null);
+    }
+
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+
+    }
+
+    @Override
+    public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
+        this.activity = binding.getActivity();
+    }
+
+    @Override
+    public void onDetachedFromActivityForConfigChanges() {
+
+    }
+
+    @Override
+    public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
+
+    }
+
+    @Override
+    public void onDetachedFromActivity() {
+
     }
 }
